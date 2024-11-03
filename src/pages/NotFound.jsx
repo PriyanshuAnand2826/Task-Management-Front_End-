932f293ae6c62c7e3a02f8d8d08248ec11ed244b
Priyanshu <priyanshuanand60@gmail.com>
2024-11-04 01:06:17 +0530

    changes done

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -8,7 +8,7 @@ export default function NotFound() {
     <div className={styles.container}>
     <h2 className={styles.sub_text}>404 ! Page not found <span>&#128531;</span></h2>
     <p style={{fontSize:'1.2rem'}}>The page you are looking for is not register by the app.</p>
-    <Link className={styles.link} to='/'>GO TO HOMEPAGE</Link>
+    <Link className={styles.link} to='/'>GO TO REGISTER PAGE</Link>
    </div>
   )
 }
